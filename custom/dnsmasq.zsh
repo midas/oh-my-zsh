117f355e0e0ca70d026ef9ada32def27acba0874
Jason Harrelson <jason.harrelson@steelpointepartners.com>
2014-04-24 16:57:11 -0500

    Add dnsmasq aliases.

---
+++ custom/dnsmasq.zsh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+alias dnsmasq-start='sudo launchctl start homebrew.mxcl.dnsmasq'
+alias dnsmasq-stop='sudo launchctl stop homebrew.mxcl.dnsmasq'
+alias dnsmasq-restart='sudo launchctl stop homebrew.mxcl.dnsmasq && sudo launchctl start homebrew.mxcl.dnsmasq'
