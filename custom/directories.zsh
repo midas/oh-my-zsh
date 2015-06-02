2178b74c9eeba9b411c7bbe95db80d8ee689f401
Jason Harrelson <jason@lookforwardenterprises.com>
2015-06-02 16:01:31 -0500

    Update directory aliases to be more current.

--- custom/directories.zsh
+++ custom/directories.zsh
@@ -1,20 +1,21 @@
-personal=/Users/cjharrelson/development/personal
-archivalock=/Users/cjharrelson/development/personal/archivalock
-gemdev=/Users/cjharrelson/development/personal/gems
-guilded=/Users/cjharrelson/development/personal/gems/guilded
-fluid_digital=/Users/cjharrelson/development/personal/fluid_digital
-hei=/Users/cjharrelson/development/personal/hei
-
-iberon=/Users/cjharrelson/development/iberon
-ncite_server=/Users/cjharrelson/development/iberon/ncite_server
-ncite_pass_control_server=/Users/cjharrelson/development/iberon/ncite_pass_control_server
-ncite_pre_registration_server=/Users/cjharrelson/development/iberon/ncite_pre_registration_server
-ncite_enterprise_server=/Users/cjharrelson/development/iberon/ncite_enterprise_server
-npcs=/Users/cjharrelson/development/iberon/ncite_pass_control_server
-device_hub=/Users/cjharrelson/development/iberon/ncite_device_hub
-gvtus=/Users/cjharrelson/development/iberon/gvt-us
 oh_my_zsh=/Users/cjharrelson/.oh-my-zsh
+oh=/Users/cjharrelson/.oh-my-zsh
 dotfiles=/Users/cjharrelson/.dotfiles
+dot=/Users/cjharrelson/.dotfiles
 rvm=/Users/cjharrelson/.rvm
-titaniumdev=/Users/cjharrelson/development/personal/titanium
-archivalockmobile=/Users/cjharrelson/development/personal/titanium/ArchivaLock
+
+# Sources
+
+## Personal
+personal=/Users/cjharrelson/sources/personal
+al=/Users/cjharrelson/sources/al
+archivalock=/Users/cjharrelson/sources/al/archivalock
+gemdev=/Users/cjharrelson/sources/personal/gems
+spp=/Users/cjharrelson/sources/spp
+
+## Iberon
+iberon=/Users/cjharrelson/sources/iberon
+ncite_pass_control_server=/Users/cjharrelson/sources/iberon/ncite_pass_control_server
+npcs=/Users/cjharrelson/sources/iberon/ncite_pass_control_server
+ncite_pre_registration_server=/Users/cjharrelson/sources/iberon/ncite_pre_registration_server
+ncite_enterprise_server=/Users/cjharrelson/sources/iberon/ncite_enterprise_server
