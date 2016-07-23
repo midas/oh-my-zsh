9524104bed4dee2446eaf7a80de4bf9c8b6ba31b
Jason Harrelson <cjharrelson@iberon.com>
2016-07-22 20:14:53 -0500

    Add xclip aliases.

--- custom/aliases.zsh
+++ custom/aliases.zsh
@@ -2,3 +2,5 @@
 
 alias crontab="VIM_CRONTAB=true crontab"
 alias yscp="rsync -e \"ssh -I /usr/local/Cellar/opensc/0.15.0/lib/opensc-pkcs11.so\" --progress"
+alias pbcopy='xclip -selection clipboard'
+alias pbpaste='xclip -selection clipboard -o'
