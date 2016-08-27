114be2db3a85a0ff2146cbf5e9fd50ea189d526f
Jason Harrelson <jason@lookforwardenterprises.com>
2016-08-27 10:47:26 -0500

    Bring custom/example.zsh in line with upstream origin/master.

--- custom/example.zsh
+++ custom/example.zsh
@@ -7,21 +7,4 @@
 #
 # brainstormr=~/Projects/development/planetargon/brainstormr
 # cd $brainstormr
-personal=/Users/cjharrelson/development/personal
-archivalock=/Users/cjharrelson/development/personal/archivalock
-gemdev=/Users/cjharrelson/development/personal/gems
-guilded=/Users/cjharrelson/development/personal/gems/guilded
-fluid_digital=/Users/cjharrelson/development/personal/fluid_digital
-hei=/Users/cjharrelson/development/personal/hei
-
-iberon=/Users/cjharrelson/development/iberon
-ncite_server=/Users/cjharrelson/development/iberon/ncite_server
-ncite_pass_control_server=/Users/cjharrelson/development/iberon/ncite_pass_control_server
-npcs=/Users/cjharrelson/development/iberon/ncite_pass_control_server
-device_hub=/Users/cjharrelson/development/iberon/ncite_device_hub
-gvtus=/Users/cjharrelson/development/iberon/gvt-us
-oh_my_zsh=/Users/cjharrelson/.oh-my-zsh
-dotfiles=/Users/cjharrelson/.dotfiles
-rvm=/Users/cjharrelson/.rvm
-titaniumdev=/Users/cjharrelson/development/personal/titanium
-archivalockmobile=/Users/cjharrelson/development/personal/titanium/ArchivaLock
+#
